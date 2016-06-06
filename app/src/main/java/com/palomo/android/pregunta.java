69fa0c07787c90f177ab18cf8280a1ca45a80755
palomo <palomox@gmail.com>
2016-06-06 13:07:05 -0500

    first commit

---
+++ app/src/main/java/com/palomo/android/pregunta.java
@@ -0,0 +1,18 @@
+package com.palomo.android;
+
+/**
+ * Created by palomo on 03/03/2016.
+ */
+public class pregunta {
+
+    import mIdResTexto : Integer
+    import mRespuestaVerdadera : boolean
+
+    const Pregunta(idResTexto : Integer, respuestaVerdadera : boolean);
+    setIdResTexto(idResTexto : Integer) : void;
+    getIdResTexto() : Integer;
+    setRespuestaVerdadera(respuestaVerdadera : boolean) : void;
+    isRespuestaVerdadera() : boolean
+
+}
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+# proyecto geoapp. una trivia sobre geografia.
